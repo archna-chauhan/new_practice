fda54bb0e2dc12a9870478bdda5bb62d7b870334
archna-chauhan <archa.23chauhan@gmail.com>
2025-04-21 22:43:18 +0530

    6 committ

--- tests/test_login.py
+++ tests/test_login.py
@@ -23,3 +23,25 @@ def test_01(setup_teardown):
     assert utility.get_page_title(driver) == "ParaBank | Accounts Overview", "Page title does not match"
 
     print(utility.get_page_title(driver))
+
+
+
+def test_02(setup_teardown):
+    driver = setup_teardown
+    login_page = LoginPage(driver)
+
+    #Way1
+    home_page = login_page.do_login(config.username_value, config.password_value)
+
+    '''
+    #Way2
+    login_page.enter_username(config.username_value)
+    login_page.enter_password(config.password_value)
+    home_page = login_page.click_login()
+    '''
+
+    time.sleep(2)
+
+    assert utility.get_page_title(driver) == "ParaBank | Accounts Overview\login", "Page title does not match"
+
+    print(utility.get_page_title(driver))
