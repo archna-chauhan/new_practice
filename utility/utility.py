b2bbcdd14b984f832d0e3b9fdc7cc15f536a85cf
archna-chauhan <archa.23chauhan@gmail.com>
2025-04-26 22:34:14 +0530

    Adding project files again

--- utility/utility.py
+++ utility/utility.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from config import config
+from selenium.webdriver.chrome.options import Options
+
+
 
 def launch_browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
     service = Service(config.chrome_driver_path)
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
 def launch_app(driver):
